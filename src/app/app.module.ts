cd5d9fcefe0cf582722a43cc60641fec67c337bb
vijayB725 <kjvj725@gmail.com>
2021-02-21 11:17:09 +0530

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { EventListComponent } from "./events/event-list/event-list.component";
+import { EventThumbnailComponent } from "./events/event-thumbnail/event-thumbnail.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    EventListComponent,
+    EventThumbnailComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/events/event-list/event-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-event-list",
+  templateUrl: "./event-list.component.html",
+  styleUrls: ["./event-list.component.css"]
+})
+export class EventListComponent implements OnInit {
+  eventList = {
+    id: 1,
+    name: "Angular connect",
+    date: "02/23/2021",
+    time: "09:30:00",
+    price: 599.99,
+    location: "Pittsburg"
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
