c9b78d821fd35da809f54d4f4cc0880be6352175
vijayB725 <kjvj725@gmail.com>
2021-02-22 14:06:11 +0530

    5 files modified

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -6,14 +6,56 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./event-list.component.css"]
 })
 export class EventListComponent implements OnInit {
-  eventList = {
-    id: 1,
-    name: "Angular connect",
-    date: "02/23/2021",
-    time: "09:30:00",
-    price: 599.99,
-    location: "Pittsburg"
-  };
+  eventList = [
+    {
+      id: 1,
+      name: "Angular connect",
+      date: "02/23/2021",
+      time: "09:00:00",
+      price: 599.99,
+      location: {
+        address: 'The Convention Center & Scuba Shop',
+        city: 'Amsterdam',
+        country: 'Netherlands'
+      }
+    },
+    {
+      id: 2,
+      name: "Angular 2.0 Event",
+      date: "02/25/2021",
+      time: "12:00",
+      price: 1000.0,
+      location: {
+        address: 'The Technology Learning Center',
+        city: 'London',
+        country: 'England'
+      }
+    },
+    {
+      id: 4,
+      name: "Ng Conference",
+      date: "02/28/2021",
+      time: "8:00:00",
+      price: 800.0,
+      location: {
+        address: 'The Adavnce Tech Institute',
+        city: 'New York',
+        country: 'United States'
+      }
+    },
+    {
+      id: 4,
+      name: "Ng Webinar",
+      date: "03/01/2021",
+      time: "09:00",
+      price: 700.0,
+      location: {
+        address: 'Marriot Prime',
+        city: 'Goa',
+        country: 'India'
+      }
+    }
+  ];
 
   constructor() {}
 
